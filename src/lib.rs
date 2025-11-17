4f6402767682dafcfb9db87f35b60cde7e6707c6
Romain Delhommais <romain@delhommais.com>
2025-11-17 23:10:10 +0100

    feat: rewrite Config and search implementations using iterators

--- src/lib.rs
+++ src/lib.rs
@@ -1,26 +1,17 @@
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut results = Vec::new();
-
-    for line in contents.lines() {
-        if line.contains(query) {
-            results.push(line);
-        }
-    }
-
-    results
+    contents
+        .lines()
+        .filter(|line| line.contains(query))
+        .collect()
 }
 
 pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut results = Vec::new();
-
     let query = &query.to_lowercase();
 
-    for line in contents.lines() {
-        if line.to_lowercase().contains(query) {
-            results.push(line);
-        }
-    }
-    results
+    contents
+        .lines()
+        .filter(move |line| line.to_lowercase().contains(query))
+        .collect()
 }
 
 #[cfg(test)]
@@ -49,7 +40,7 @@ body
 adult
 ";
 
-        assert_eq!(vec!["nobody", "somebody", "body"], search(query, contents))
+        assert_eq!(vec!["nobody", "somebody", "body"], search(query, contents));
     }
 
     #[test]
